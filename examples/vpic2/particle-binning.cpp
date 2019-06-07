18b6b5ccd0abcba9fb95bd9ca2ea5cf1b9ee37ac
dbin_sdm <dbin@lbl.gov>
2019-06-07 13:50:12 -0700

    add particle-binning.cp

--- examples/vpic2/particle-binning.cpp
+++ examples/vpic2/particle-binning.cpp
@@ -9,37 +9,37 @@
 #include <math.h> /* ceil  and floor*/
 #include <cstring>
 #include <optional> //Need c++17
-
+#include <limits>
 #include "array_udf.h"
 
 using namespace std;
 
 struct Particle
 {
-    float x;
-    float y;
-    float z;
+    float Ux;
+    float Uy;
+    float Uz;
     friend std::ostream &operator<<(std::ostream &os, Particle &p) //test
     {
-        os << p.x << '/' << p.y << '/' << p.z;
+        os << p.Ux << '/' << p.Uy << '/' << p.Uz;
         return os;
     }
     friend bool operator==(Particle &lp, Particle &rp)
     {
-        return (lp.x == rp.x && lp.y == rp.y && lp.z == rp.z);
+        return (lp.Ux == rp.Ux && lp.Uy == rp.Uy && lp.Uz == rp.Uz);
     }
     void set_value(int index, float value)
     {
         switch (index)
         {
         case 0:
-            x = value;
+            Ux = value;
             break;
         case 1:
-            y = value;
+            Uy = value;
             break;
         case 2:
-            z = value;
+            Uz = value;
             break;
         default:
             printf("The object only has three variables !\n");
@@ -53,13 +53,13 @@ struct Particle
         switch (index)
         {
         case 0:
-            return x;
+            return Ux;
             break;
         case 1:
-            return y;
+            return Uy;
             break;
         case 2:
-            return z;
+            return Uz;
             break;
         default:
             printf("The object only has three variables !\n");
@@ -69,17 +69,33 @@ struct Particle
     }
 };
 
+int n_bins = 10;
+float local_max = std::numeric_limits<float>::min(), local_min = std::numeric_limits<float>::max();
+float global_max, global_min, hist_interval;
+std::vector<int> local_hist(n_bins, 0), global_hist(n_bins, 0);
+
 //Find the global Z
-inline std::optional<Particle> Filter_UDF(const Stencil<Particle> &p)
+inline int MinMax_UDF(const Stencil<Particle> &p)
 {
-    if (p(0).x > 200.0 && p(0).y > 0.0 && p(0).z > 0.0)
+    if (p(0).Ux > local_max)
     {
-        return p(0);
+        local_max = p(0).Ux;
     }
-    else
+
+    if (p(0).Ux < local_min)
     {
-        return {};
+        local_min = p(0).Ux;
     }
+
+    return 0;
+}
+
+//Find the global Z
+inline int Binning_UDF(const Stencil<Particle> &p)
+{
+    int bin_index = floor((p(0).Ux - global_min) / hist_interval);
+    local_hist[bin_index] = local_hist[bin_index] + 1;
+    return 0;
 }
 
 int main(int argc, char *argv[])
@@ -98,22 +114,25 @@ int main(int argc, char *argv[])
 
     Array<Particle, float> *P = new Array<Particle, float>(AU_VIRTUAL);
 
-    P->PushBackAttribute(AU_NVS, AU_HDF5, p_file, group, "x", 0);
-    P->PushBackAttribute(AU_NVS, AU_HDF5, p_file, group, "y", 0);
-    P->PushBackAttribute(AU_NVS, AU_HDF5, p_file, group, "z", 0);
+    P->PushBackAttribute(AU_NVS, AU_HDF5, p_file, group, "Ux", 0);
+    P->PushBackAttribute(AU_NVS, AU_HDF5, p_file, group, "Uy", 0);
+    P->PushBackAttribute(AU_NVS, AU_HDF5, p_file, group, "Uz", 0);
 
-    Array<Particle, float> *PF = new Array<Particle, float>(AU_VIRTUAL);
+    P->Apply(MinMax_UDF);
+    MPI_Allreduce(&local_max, &global_max, 1, MPI_FLOAT, MPI_MAX, MPI_COMM_WORLD);
+    MPI_Allreduce(&local_min, &global_min, 1, MPI_FLOAT, MPI_MAX, MPI_COMM_WORLD);
 
-    PF->PushBackAttribute(AU_COMPUTED, AU_HDF5, o_file, group, "x", 0);
-    PF->PushBackAttribute(AU_COMPUTED, AU_HDF5, o_file, group, "y", 0);
-    PF->PushBackAttribute(AU_COMPUTED, AU_HDF5, o_file, group, "z", 0);
+    hist_interval = (global_max - global_min) / n_bins;
 
-    //P->SetApplyOptionalOutput();
-    P->Apply(Filter_UDF, PF);
+    P->Apply(Binning_UDF);
+
+    MPI_Gather(&local_hist[0], n_bins, MPI_INT, &global_hist[0], n_bins, MPI_INT, 0, MPI_COMM_WORLD);
+    for (int i = 0; i < n_bins; i++)
+        std::cout << i << " : " << global_hist[i] << "\n";
+
     P->ReportTime();
 
     delete P;
-    delete PF;
 
     MPI_Finalize();
     return 0;
