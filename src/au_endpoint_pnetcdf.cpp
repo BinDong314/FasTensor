4f9ae76623e864aa2bc193d10daadc0a4f08eaaf
dbin_sdm <dbin@lbl.gov>
2020-06-05 11:01:21 -0700

    add pnetcodf

---
+++ src/au_endpoint_pnetcdf.cpp
@@ -0,0 +1,211 @@
+
+#include "au_endpoint_pnetcdf.h"
+
+#ifdef HAS_PNETCDF_END_POINT
+
+static void handle_pnetcdf_error(int status, int lineno)
+{
+    fprintf(stderr, "Error at line %d: %s\n", lineno, ncmpi_strerror(status));
+    MPI_Abort(MPI_COMM_WORLD, 1);
+}
+
+int EndpointPnetCDF::ParseEndpointInfo()
+{
+    //std::cout << endpoint_info << "\n";
+    std::stringstream ss(endpoint_info);
+    if (!std::getline(ss, fn_str, ':'))
+    {
+        AU_EXIT("Invalued endpoint_info");
+    }
+
+    if (!std::getline(ss, vn_str, ':'))
+    {
+        AU_EXIT("Invalued endpoint_info");
+    }
+
+    std::cout << "fn_str =" << fn_str << ", vn_str = " << vn_str << std::endl;
+    return 0;
+}
+
+//bindings/C/adios2/c/adios2_c_types.h
+void EndpointPnetCDF::Map2MyType()
+{
+    switch (data_element_type)
+    {
+    case AU_SHORT:
+        element_type_mpi = MPI_SHORT;
+        element_type_nc = NC_SHORT;
+        return;
+    case AU_INT:
+        element_type_mpi = MPI_INT;
+        element_type_nc = NC_INT;
+        return;
+    case AU_LONG:
+        element_type_mpi = MPI_LONG;
+        element_type_nc = NC_LONG;
+        return;
+    case AU_LONG_LONG:
+        element_type_mpi = MPI_LONG_LONG;
+        element_type_nc = NC_LONG;
+        return;
+    case AU_USHORT:
+        element_type_mpi = MPI_UNSIGNED_SHORT;
+        element_type_nc = NC_USHORT;
+        return;
+    case AU_UINT:
+        element_type_mpi = MPI_UNSIGNED;
+        element_type_nc = NC_UINT;
+        return;
+    case AU_ULONG:
+        element_type_mpi = MPI_UNSIGNED_LONG;
+        element_type_nc = NC_UINT64;
+        return;
+    case AU_ULLONG:
+        element_type_mpi = MPI_UINT64_T;
+        element_type_nc = NC_UINT64;
+        return;
+    case AU_FLOAT:
+        element_type_mpi = MPI_FLOAT;
+        element_type_nc = NC_FLOAT;
+        return;
+    case AU_DOUBLE:
+        element_type_mpi = MPI_DOUBLE;
+        element_type_nc = NC_DOUBLE;
+        return;
+    default:
+        std::cout << "Unsupported datatype in " << __FILE__ << " : " << __LINE__ << std::endl;
+        std::flush(std::cout);
+        std::exit(EXIT_FAILURE);
+    }
+};
+
+/**
+     * @brief print information about the endpoint
+     * 
+     * @return < 0 error, >= 0 works 
+     */
+int EndpointPnetCDF::PrintInfo()
+{
+    std::cout << "fn_str : " << fn_str << ", vn_str :" << vn_str << "\n";
+    return 0;
+}
+
+/**
+    * @brief extracts metadata, possbile endpoint_ranks/endpoint_dim_size/data_element_type
+    * 
+    * @return int < 0 error, >= 0 works 
+    */
+int EndpointPnetCDF::ExtractMeta()
+{
+    return 0;
+}
+
+/**
+     * @brief write the data to the end-point
+     * 
+     * @param start, coordinates of the cell to start (including)
+     * @param end , coordinates of the cell to end (including)
+     * @param data, store the result data 
+     * @return int < 0 error, >= 0 works
+     */
+int EndpointPnetCDF::Write(std::vector<unsigned long long> start, std::vector<unsigned long long> end, void *data)
+{
+    Map2MyType();
+
+    std::vector<MPI_Offset> pnetcdf_start, pnetcdf_count;
+    int rank = start.size();
+    pnetcdf_start.resize(rank);
+    pnetcdf_count.resize(rank);
+
+    MPI_Offset element_count = 1;
+    for (int i = 0; i < rank; i++)
+    {
+        pnetcdf_start[i] = 0;
+        pnetcdf_count[i] = endpoint_dim_size[i] - 1;
+        pnetcdf_element_count = pnetcdf_count[i] * pnetcdf_element_count;
+    }
+
+    int ret = ncmpi_put_vara_all(ncfile, ncvar, &pnetcdf_start[0], &pnetcdf_count[0], data, pnetcdf_element_count, element_type_mpi);
+    if (ret != NC_NOERR)
+        handle_pnetcdf_error(ret, __LINE__);
+}
+
+/**
+     * @brief read the data from end-point
+     * 
+     * @param start, coordinates of the cell to start (including)
+     * @param end , coordinates of the cell to end (including)
+     * @param data, store the result data 
+     * @return int < 0 error, >= 0 works
+     */
+int EndpointPnetCDF::Read(std::vector<unsigned long long> start, std::vector<unsigned long long> end, void *data)
+{
+    Map2MyType();
+
+    std::vector<MPI_Offset> pnetcdf_start, pnetcdf_count;
+    int rank = start.size();
+    pnetcdf_start.resize(rank);
+    pnetcdf_count.resize(rank);
+
+    MPI_Offset element_count = 1;
+    for (int i = 0; i < rank; i++)
+    {
+        pnetcdf_start[i] = 0;
+        pnetcdf_count[i] = endpoint_dim_size[i] - 1;
+        pnetcdf_element_count = pnetcdf_count[i] * pnetcdf_element_count;
+    }
+
+    int ret = ncmpi_get_vara_all(ncfile, ncvar, &pnetcdf_start[0], &pnetcdf_count[0], data, pnetcdf_element_count, element_type_mpi);
+    if (ret != NC_NOERR)
+        handle_pnetcdf_error(ret, __LINE__);
+    return 0;
+}
+
+int EndpointPnetCDF::Create()
+{
+    int ret;
+    ret = ncmpi_create(MPI_COMM_WORLD_DEFAULT, fn_str.c_str(), NC_CLOBBER | NC_64BIT_OFFSET, MPI_INFO_NULL_DEFAULT, &ncfile);
+    if (ret != NC_NOERR)
+        handle_pnetcdf_error(ret, __LINE__);
+
+    int rank = endpoint_dim_size.size();
+
+    std::vector<int> dimid;
+    dimid.resize(rank);
+
+    for (int i = 0; i < rank; i++)
+    {
+        ret = ncmpi_def_dim(ncfile, "somename", endpoint_dim_size[i], &dimid[i]);
+        if (ret != NC_NOERR)
+            handle_pnetcdf_error(ret, __LINE__);
+    }
+    ret = ncmpi_def_var(ncfile, vn_str.c_str(), element_type_nc, rank, &dimid, &ncvar);
+    if (ret != NC_NOERR)
+        handle_pnetcdf_error(ret, __LINE__);
+
+    ret = ncmpi_enddef(ncfile);
+    if (ret != NC_NOERR)
+        handle_pnetcdf_error(ret, __LINE__);
+
+    return 0;
+}
+
+int EndpointPnetCDF::Open()
+{
+    int ret = ncmpi_open(MPI_COMM_WORLD_DEFAULT, fn_str.c_str(), NC_WRITE, MPI_INFO_NULL_DEFAULT,
+                         &ncfile);
+    if (ret != NC_NOERR)
+        handle_pnetcdf_error(ret, __LINE__);
+
+    ret = ncmpi_inq_varid(ncfile, ncvar.c_str(), &ncvar);
+    if (ret != NC_NOERR)
+        handle_pnetcdf_error(ret, __LINE__);
+    return ret;
+}
+
+int EndpointPnetCDF::Close()
+{
+    return ncmpi_close(ncfile);
+}
+
+#endif
---
+++ src/au_endpoint_pnetcdf.h
@@ -0,0 +1,179 @@
+/**
+ *ArrayUDF Copyright (c) 2017, The Regents of the University of California, through Lawrence Berkeley National Laboratory (subject to receipt of any required approvals from the U.S. Dept. of Energy).  All rights reserved.
+ *
+ *If you have questions about your rights to use or distribute this software, please contact Berkeley Lab's Innovation & Partnerships Office at  IPO@lbl.gov.
+ *
+ * NOTICE. This Software was developed under funding from the U.S. Department of Energy and the U.S. Government consequently retains certain rights. As such, the U.S. Government has been granted for itself and others acting on its behalf a paid-up, nonexclusive, irrevocable, worldwide license in the Software to reproduce, distribute copies to the public, prepare derivative works, and perform publicly and display publicly, and to permit other to do so. 
+ *
+ */
+
+/**
+ *
+ * Email questions to dbin@lbl.gov
+ * Scientific Data Management Research Group
+ * Lawrence Berkeley National Laboratory
+ *
+ */
+
+#ifndef END_POINT_PNETCDF_H
+#define END_POINT_PNETCDF_H
+
+#include "au_type.h"
+#include "au_endpoint.h"
+#include <string>
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+#ifdef HAS_PNETCDF_END_POINT
+
+#include "pnetcdf.h"
+
+class EndpointPnetCDF : public Endpoint
+{
+private:
+    std::string fn_str, vn_str;    //file name and variable name
+    MPI_Datatype element_type_mpi; //PnetCDF use MPI type
+    nc_type element_type_nc;       //PnetCDF's native type
+    int ncfile, ncvar;
+
+public:
+    /**
+     * @brief Construct a new EndpointHDF5 object
+     * 
+     * @param data_endpoint contains the info of the endpoint, e.g., file type + file info
+     */
+    EndpointPnetCDF(std::string endpoint_info_p)
+    {
+        endpoint_info = endpoint_info_p;
+        ParseEndpointInfo();
+
+        SetOpenFlag(false);
+        SetCreateFlag(false);
+    }
+    ~EndpointPnetCDF()
+    {
+    }
+    /**
+     * @brief extracts metadata, possbile endpoint_ranks/endpoint_dim_size/data_element_type
+     * 
+     * @return int < 0 error, >= 0 works 
+     */
+    int ExtractMeta() override;
+    /**
+     * @brief print information about the endpoint
+     * 
+     * @return < 0 error, >= 0 works 
+     */
+    int PrintInfo() override;
+
+    /**
+     * @brief create the endpoint
+     * 
+     * @return  < 0 error, >= 0 works 
+     */
+    int Create() override;
+
+    /**
+     * @brief open the endpoint
+     * 
+     * @return < 0 error, >= 0 works 
+     */
+    int Open() override;
+
+    /**
+     * @brief read the data from end-point
+     * 
+     * @param start, coordinates of the cell to start (including)
+     * @param end , coordinates of the cell to end (including)
+     * @param data, store the result data 
+     * @return int < 0 error, >= 0 works
+     */
+    int Read(std::vector<unsigned long long> start, std::vector<unsigned long long> end, void *data) override;
+
+    /**
+     * @brief write the data to the end-point
+     * 
+     * @param start, coordinates of the cell to start (including)
+     * @param end , coordinates of the cell to end (including)
+     * @param data, store the result data 
+     * @return int < 0 error, >= 0 works
+     */
+    int Write(std::vector<unsigned long long> start, std::vector<unsigned long long> end, void *data) override;
+
+    /**
+     * @brief close the end-point
+     * 
+     * @return int int < 0 error, >= 0 works
+     */
+    int Close() override;
+
+    void Map2MyType() override;
+
+    int ParseEndpointInfo() override;
+};
+#else
+class EndpointPnetCDF : public Endpoint
+{
+private:
+public:
+    /**
+     * @brief Construct a new EndpointHDF5 object
+     * 
+     * @param data_endpoint contains the info of the endpoint, e.g., file type + file info
+     */
+    EndpointPnetCDF(std::string endpoint_info_p)
+    {
+        std::cout << "EndpointADIOS is not configured and compiled ! \n";
+    }
+    ~EndpointPnetCDF()
+    {
+        Close();
+    }
+
+    int ParseEndpointInfo() override
+    {
+        return -1;
+    }
+
+    int ExtractMeta() override
+    {
+        return -1;
+    }
+
+    int PrintInfo() override
+    {
+        return -1;
+    }
+
+    int Create() override
+    {
+        return -1;
+    }
+
+    int Open() override
+    {
+        return -1;
+    }
+
+    void Map2MyType() override
+    {
+    }
+
+    int Read(std::vector<unsigned long long> start, std::vector<unsigned long long> end, void *data) override
+    {
+        return -1;
+    }
+
+    int Write(std::vector<unsigned long long> start, std::vector<unsigned long long> end, void *data) override
+    {
+        return -1;
+    }
+
+    int Close() override
+    {
+        return -1;
+    }
+};
+#endif
+#endif
