230daf4b6ff3e2b99eebde68e094cfa0b5f1abe0
Bin Dong <dbin@bins-mbp.attlocal.net>
2021-06-30 22:04:51 -0700

    update to test cache

--- src/ft_stencil.h
+++ src/ft_stencil.h
@@ -783,7 +783,7 @@ public:
       chunk_dim_size_3d_temp_2 = chunk_dim_size_3d_temp * start_offset[0] + chunk_dim_size[2] * start_offset[1];
       //count_size_t_size = sizeof(T) * count_size_t[2];
       count_size_t_size = count_size_t[2];
-      for (int i = 0; i < count_size_t[0]; i++)
+      /*for (int i = 0; i < count_size_t[0]; i++)
       {
         //array_buffer_offset = chunk_dim_size_3d_temp * (i + start_offset[0]);
         memcpy_src = chunk_data_pointer + chunk_dim_size_3d_temp * i + chunk_dim_size_3d_temp_2;
@@ -804,7 +804,19 @@ public:
         memcpy_double(memcpy_dst, memcpy_src, count_size_t_size);
         memcpy_double(memcpy_dst, memcpy_src, count_size_t_size);
         memcpy_double(memcpy_dst, memcpy_src, count_size_t_size);
-      }
+      }*/
+      //just some test code for cache effect
+      memcpy_src = chunk_data_pointer + chunk_dim_size_3d_temp_2;
+      memcpy_dst = rv.data();
+      memcpy_double(memcpy_dst, memcpy_src, count_size_t_size);
+      memcpy_double(memcpy_dst, memcpy_src, count_size_t_size);
+      memcpy_double(memcpy_dst, memcpy_src, count_size_t_size);
+      memcpy_double(memcpy_dst, memcpy_src, count_size_t_size);
+      memcpy_double(memcpy_dst, memcpy_src, count_size_t_size);
+      memcpy_double(memcpy_dst, memcpy_src, count_size_t_size);
+      memcpy_double(memcpy_dst, memcpy_src, count_size_t_size);
+      memcpy_double(memcpy_dst, memcpy_src, count_size_t_size);
+      memcpy_double(memcpy_dst, memcpy_src, count_size_t_size);
       return 0;
     }
     ArrayViewAccessP<T>(rv.data(), chunk_data_pointer, chunk_dim_size, view_start, view_end, ARRAY_VIEW_READ);
