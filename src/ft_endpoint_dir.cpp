1011695b44bc48f38d09765dd9c28cd44b45e54d
Bin Dong <dbin@bins-mbp.attlocal.net>
2022-04-15 09:13:54 -0700

    Disable test

--- src/ft_endpoint_dir.cpp
+++ src/ft_endpoint_dir.cpp
@@ -442,8 +442,10 @@ int EndpointDIR::Read(std::vector<unsigned long long> start, std::vector<unsigne
 
     for (int i = sub_endpoint_index; i < sub_endpoint_index_end; i++)
     {
-        // sub_endpoint->SetEndpointInfo(dir_str + "/" + dir_file_list[i] + ":" + append_sub_endpoint_info);
+// sub_endpoint->SetEndpointInfo(dir_str + "/" + dir_file_list[i] + ":" + append_sub_endpoint_info);
+#ifdef DEBUG
         std::cout << "Read = dir_file_list[" << i << "] = " << dir_file_list[i] << " , append_sub_endpoint_info =" << append_sub_endpoint_info << "\n";
+#endif
         sub_endpoint->SetEndpointInfo(dir_file_list[i] + ":" + append_sub_endpoint_info);
         sub_endpoint->Open();
         sub_endpoint->Read(start_sub_endpoint, end_sub_endpoint, data_temp);
